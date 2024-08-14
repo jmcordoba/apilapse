5bf21b68750413bed6182c1a353d20b85dd4fe6b
Juanma Córdoba <jmcordoba@gmail.com>
2024-08-14 17:03:09 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import logging, logging.handlers
+
+# https://kb.synology.com/en-uk/DSM/tutorial/Set_up_Python_virtual_environment_on_NAS
+# https://medium.com/@rizqinur2010/deploying-python-flask-in-synology-dsm-7-without-docker-d99f1603bc87
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+
+    logger = logging.getLogger()
+
+    h = logging.handlers.SysLogHandler(address=("jmcordoba.synology.me", 514), facility='user')
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(h)
+    logger.info('message from main module')
+
+    return "Hello, World!"
+
+@app.route("/x")
+def hellox():
+
+    # https://api.twitter.com/2/openapi.json
+
+    return "Hello, X!"
