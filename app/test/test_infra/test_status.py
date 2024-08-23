8f8f7af75e06c9038eb27eb75a75a53b0774380e
JuanMa Córdoba Hernández <jmcordoba@JmMacBookPro.local>
2024-08-23 17:47:53 +0200

    adding status endpoint and test

--- app/test/test_infra/test_status.py
+++ app/test/test_infra/test_status.py
@@ -3,21 +3,7 @@ blablabla
 """
 import unittest
 from unittest.mock import MagicMock
-from dataclasses import dataclass
 from src.infra.status import Status
-
-@dataclass
-class FakeRequest:
-    """
-    test/infra/status/fake_request
-    """
-    def __init__(self, method, remote_addr, user_agent):
-        """
-        test/infra/status/fake_request
-        """
-        self.method = method
-        self.remote_addr = remote_addr
-        self.user_agent = user_agent
 
 class TestInfraStatus(unittest.TestCase):
     """
@@ -27,13 +13,16 @@ class TestInfraStatus(unittest.TestCase):
         """
         test/infra/status/test_get_status
         """
-        mock_object = MagicMock()
-        mock_object.user_agent = "fake_user_agent"
+        mock_user_agent = MagicMock()
+        mock_user_agent.user_agent = "fake_user_agent"
 
-        fake_request = FakeRequest('get', '7.7.7.7', mock_object)
+        mock_request = MagicMock()
+        mock_request.method = 'get'
+        mock_request.remote_addr = '7.7.7.7'
+        mock_request.user_agent = mock_user_agent
 
         status = Status()
-        result = status.get_status(fake_request)
+        result = status.get_status(mock_request)
 
         self.assertIsNot(result, 1)
 
