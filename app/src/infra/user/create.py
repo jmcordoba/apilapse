40e98d6a1e6c238688bb0e6f7d363f1833af224a
Juanma Córdoba <jmcordoba@gmail.com>
2024-11-17 17:19:15 +0100

    including some test files

---
+++ app/src/infra/user/create.py
@@ -0,0 +1,28 @@
+"""
+blablabla
+"""
+import os
+from flask import request
+from src.infra.sqlite3 import Database  # Import the Database class
+
+
+class UserCreate:
+    """
+    blablabla
+    """
+
+    def insert_user(self):
+        # Get the database name from the environment and Initialize the database
+        db = Database(os.getenv('database_name'))
+        db.create_connection()
+
+        # Insert a new row
+        name = request.json.get('name')
+        email = request.json.get('email')
+        insert_query = "INSERT INTO users (name, email) VALUES (?, ?)"
+        db.execute_query(insert_query, (name, email))
+
+        # Close connection
+        db.close_connection()
+
+        return "Database initialized and new row inserted"
