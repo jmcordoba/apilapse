8a4bdc8a90fc96f34fafd8903e9f19454557b40b
Juanma Córdoba <jmcordoba@gmail.com>
2024-11-17 17:53:38 +0100

    including some bach files

--- bash/lint.sh
+++ bash/lint.sh
@@ -1,1 +1,1 @@
-find . -type f -name "*.py" | grep -v ".venv" | grep -v "app/test/" | xargs pylint --fail-under=9
+find . -type f -name "*.py" | grep -v ".venv" | grep -v "test/" | grep -v "app/test/" | xargs pylint --fail-under=9
