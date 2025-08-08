f6ab6b2b8b1acee7299c81f25d5b21009e89716e
JuanMa Córdoba Hernández <jmcordoba@gmail.com>
2025-08-08 16:27:49 +0200

    routes requests enhanced

--- app/routes/requests.py
+++ app/routes/requests.py
@@ -4,7 +4,7 @@ from src.app.request.update import RequestUpdate
 from src.app.request.get import RequestGet
 from src.app.request.delete import RequestDelete
 
-from exceptions import AuthenticationValidationError
+from exceptions import RequestValidationError, UserValidationError, AuthenticationValidationError
 
 req = Blueprint('req', __name__)
 
@@ -13,48 +13,66 @@ def requests_v1_create_request():
     """
     Create a new request
     """
-    request_create = RequestCreate()
-    response = request_create.create(request)
 
-    if isinstance(response, tuple):
-        data, status_code = response
-    else:
-        data = response
-        status_code = 200
+    try:
+        request_create = RequestCreate()
+        response = request_create.create(request)
 
-    return jsonify(data), status_code, {'Access-Control-Allow-Origin':'*'}
+        return jsonify(response), 201, {'Access-Control-Allow-Origin':'*'}
+    
+    except UserValidationError as e:
+        print(f"UserValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except RequestValidationError as e:
+        print(f"RequestValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500, {'Access-Control-Allow-Origin':'*'}
 
 @req.route("/request/<string:request_uuid>", methods=['PUT'])
 def requests_v1_update_request_by_id(request_uuid):
     """
     Update a request
     """
-    request_update = RequestUpdate()
-    response = request_update.update(request, request_uuid)
 
-    if isinstance(response, tuple):
-        data, status_code = response
-    else:
-        data = response
-        status_code = 200
+    try:
+        request_update = RequestUpdate()
+        response = request_update.update(request, request_uuid)
 
-    return jsonify(data), status_code, {'Access-Control-Allow-Origin':'*'}
+        return jsonify(response), 200, {'Access-Control-Allow-Origin':'*'}
+    
+    except UserValidationError as e:
+        print(f"UserValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except RequestValidationError as e:
+        print(f"RequestValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500, {'Access-Control-Allow-Origin':'*'}
 
 @req.route("/request/<string:request_uuid>", methods=['GET'])
 def requests_v1_get_request_by_id(request_uuid):
     """
     Get request by UUID
     """
-    request_get = RequestGet()
-    response = request_get.get(request, request_uuid)
 
-    if isinstance(response, tuple):
-        data, status_code = response
-    else:
-        data = response
-        status_code = 200
+    try:
+        request_get = RequestGet()
+        response = request_get.get(request, request_uuid)
 
-    return jsonify(data), status_code, {'Access-Control-Allow-Origin':'*'}
+        return jsonify(response), 200, {'Access-Control-Allow-Origin':'*'}
+    
+    except AuthenticationValidationError as e:
+        print(f"AuthenticationValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except UserValidationError as e:
+        print(f"UserValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except RequestValidationError as e:
+        print(f"RequestValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500, {'Access-Control-Allow-Origin':'*'}
 
 @req.route("/all", methods=['GET'])
 def requests_v1_get_all():
@@ -66,28 +84,39 @@ def requests_v1_get_all():
         request_get = RequestGet()
         response = request_get.get_all(request)
 
-        if isinstance(response, tuple):
-            data, status_code = response
-        else:
-            data = response
-            status_code = 200
-
-        return jsonify(data), status_code, {'Access-Control-Allow-Origin':'*'}
+        return jsonify(response), 200, {'Access-Control-Allow-Origin':'*'}
+    
     except AuthenticationValidationError as e:
-        return {"message": str(e)}, 400
+        print(f"AuthenticationValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except UserValidationError as e:
+        print(f"UserValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except RequestValidationError as e:
+        print(f"RequestValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500, {'Access-Control-Allow-Origin':'*'}
 
 @req.route("/request/<string:request_uuid>", methods=['DELETE'])
 def requests_v1_delete_request_by_id(request_uuid):
     """
     Get request by UUID
     """
-    request_delete = RequestDelete()
-    response = request_delete.delete(request, request_uuid)
+    try:
+        request_delete = RequestDelete()
+        response = request_delete.delete(request, request_uuid)
 
-    if isinstance(response, tuple):
-        data, status_code = response
-    else:
-        data = response
-        status_code = 200
-
-    return jsonify(data), status_code, {'Access-Control-Allow-Origin':'*'}
+        return jsonify(response), 200, {'Access-Control-Allow-Origin':'*'}
+    
+    except AuthenticationValidationError as e:
+        print(f"AuthenticationValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except UserValidationError as e:
+        print(f"UserValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except RequestValidationError as e:
+        print(f"RequestValidationError: {str(e)}")
+        return {"message": str(e)}, 401
+    except Exception as e:
+        return jsonify({"message": str(e)}), 500, {'Access-Control-Allow-Origin':'*'}
--- app/src/app/request/update.py
+++ app/src/app/request/update.py
@@ -4,6 +4,8 @@ from src.infra.shared.authentication import Authentication
 from src.infra.shared.body_params import BodyParams
 from src.infra.request.sqlite import Request
 
+from exceptions import RequestValidationError
+
 @dataclass
 class RequestUpdate:
     """
@@ -13,50 +15,55 @@ class RequestUpdate:
         """
         Update a request into the database.
         """
-        # Get the user UUID from the Access Token
-        auth = Authentication()
-        user_uuid = auth.get_user_uuid_from_access_token(request)
+        try:
+            # Get the user UUID from the Access Token
+            auth = Authentication()
+            user_uuid = auth.get_user_uuid_from_access_token(request)
 
-        # Get the user UUID from the Access Token
-        req = Request()
-        account_uuid = req.get_account_uuid_from_user_uuid(user_uuid)
+            # Get the user UUID from the Access Token
+            req = Request()
+            account_uuid = req.get_account_uuid_from_user_uuid(user_uuid)
 
-        # Get the account UUID from the request UUID
-        account = req.get_account_uuid_from_request_uuid(request_uuid, account_uuid)
-        if not account:
-            return {"message": "No matching request found for the given account_uuid and request_uuid"}, 404
+            # Get the account UUID from the request UUID
+            account = req.get_account_uuid_from_request_uuid(request_uuid, account_uuid)
+            if not account:
+                raise RequestValidationError("No matching request found for the given account_uuid and request_uuid")
 
-        # Get the JSON body parameters
-        body_params = BodyParams()
+            # Get the JSON body parameters
+            body_params = BodyParams()
 
-        active = body_params.get_active(request)
-        if not isinstance(active, int) or active not in [0, 1]:
-            return {"message": active["message"]}, 400
+            active = body_params.get_active(request)
+            if not isinstance(active, int) or active not in [0, 1]:
+                raise RequestValidationError("message: ", active["message"])
 
-        periodicity = body_params.get_periodicity(request)
-        name = body_params.get_name(request)
-        url = body_params.get_url(request)
-        method = body_params.get_method(request)
-        headers = body_params.get_headers(request)
-        user_agent = body_params.get_user_agent(request)
-        authentication = body_params.get_authentication(request)
-        credentials = body_params.get_credentials(request)
-        body = body_params.get_body(request)
-        tags = body_params.get_tags(request)
+            periodicity = body_params.get_periodicity(request)
+            name = body_params.get_name(request)
+            url = body_params.get_url(request)
+            method = body_params.get_method(request)
+            headers = body_params.get_headers(request)
+            user_agent = body_params.get_user_agent(request)
+            authentication = body_params.get_authentication(request)
+            credentials = body_params.get_credentials(request)
+            body = body_params.get_body(request)
+            tags = body_params.get_tags(request)
 
-        # Create the request in DB
-        data = req.update_request(
-            request_uuid, 
-            active, 
-            periodicity, 
-            name, 
-            url, 
-            method, 
-            headers, 
-            user_agent, 
-            authentication, 
-            credentials, 
-            body, 
-            tags)
+            # Create the request in DB
+            data = req.update_request(
+                request_uuid, 
+                active, 
+                periodicity, 
+                name, 
+                url, 
+                method, 
+                headers, 
+                user_agent, 
+                authentication, 
+                credentials, 
+                body, 
+                tags)
 
-        return data
+            return data
+        
+        except RequestValidationError as e:
+            print(f"RequestValidationError: {str(e)}")
+            raise RequestValidationError(f"message: {str(e)}")
